30e270af31f33e8fc874a0454e3d6cd9efd89af4
travis-russell <russell.travis@yahoo.com>
2019-02-04 15:27:39 -0600

    trying pg.connect

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const knexConfig = require('./knexfile.js');
 const db = knex(knexConfig.development);
 const cors = require('cors');
 const port = process.env.PORT || 9000;
+const pg = require('pg');
+require('dotenv').config();
+const environment = process.env.ENVIRONMENT || 'development';
 
 server.use(helmet(), cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.status(200).send('Hi!');
+	pg.connect(process.env.DATABASE_URL, () => {
+		res.status(200).send('Hi!');
+	});
 });
 
 server.listen(port, () => {
-    console.log(`Server started on ${port}`);
-})
+	console.log(`Server started on ${port}`);
+});
