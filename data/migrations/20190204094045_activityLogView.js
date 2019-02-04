76ed70d83b759e773493e9708a97546109595914
travis-russell <russell.travis@yahoo.com>
2019-02-04 12:03:41 -0600

    create activity table

---
+++ data/migrations/20190204094045_activityLogView.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('activity', (table) => {
+		table.increments();
+		table.string('name', 128).notNullable().references('id').inTable('users');
+
+		table.integer('enjoymentLevel').notNullable();
+		table.integer('energyLevel').notNullable();
+		table.text('engagement').notNullable();
+		table.timestamp('timestamp').defaultTo(knex.fn.now());
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('activity');
+};
