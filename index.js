1e555623bad37a0d21f3ead29274bbe15ed9c8a1
travis-russell <russell.travis@yahoo.com>
2019-02-04 16:07:39 -0600

    deleted pg.connect

--- index.js
+++ index.js
@@ -17,28 +17,6 @@ server.get('/', (req, res) => {
 	res.status(200).send('Hi!');
 });
 
-const { Pool, Client } = require('pg');
-const connectionString = DATABASE_URL;
-
-const pool = new Pool({
-	connectionString: connectionString
-});
-
-pool.query('SELECT NOW()', (err, res) => {
-	console.log(err, res);
-	pool.end();
-});
-
-const client = new Client({
-	connectionString: connectionString
-});
-client.connect();
-
-client.query('SELECT NOW()', (err, res) => {
-	console.log(err, res);
-	client.end();
-});
-
 server.listen(port, () => {
 	console.log(`Server started on ${port}`);
 });
