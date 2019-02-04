23e10ead7e1d4696daf3aca8daa6077653465bd6
travis-russell <russell.travis@yahoo.com>
2019-02-04 17:36:57 -0600

    reflection table fk

--- data/migrations/20190204094057_reflectionLog.js
+++ data/migrations/20190204094057_reflectionLog.js
@@ -1,8 +1,8 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('reflection', (table) => {
 		table.increments();
-		table.string('week', 32).notNullable().references('id').inTable('users');
-
+		table.string('week', 32).notNullable();
+		table.integer('fk').notNullable().references('id').inTable('users');
 		table.string('journalEntry').notNullable();
 		table.text('insights').notNullable();
 		table.text('trends').notNullable();
