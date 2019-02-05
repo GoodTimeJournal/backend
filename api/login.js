ced73e504d56b6f5e18a55f6dd18cb82a7223e71
travis-russell <russell.travis@yahoo.com>
2019-02-05 12:24:38 -0600

    auth

--- api/login.js
+++ api/login.js
@@ -1,11 +1,50 @@
-// const db = knex(knexConfig.development)
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const db = require('../data/helper/userModal');
+const { authenticate, generateToken } = require('../auth/auth');
 
-// server.post('/register', (req, res) => {
-// 	const { name, password } = req.body;
-// 	db.create(users)
-// });
-
-server.post('/login', (req, res) => {
-	const { username } = req.body;
-	res.status(200).send('Login here');
+router.post('/register', (req, res) => {
+	const creds = req.body;
+	const hash = bcrypt.hashSync(creds.password, 14);
+	creds.password = hash;
+	// console.log(creds);
+	db
+		.createUser(creds)
+		.then((id) => {
+			console.log(id);
+			db
+				.findUserId(id)
+				.then((user) => {
+					console.log(user);
+					const token = generateToken(user);
+					console.log(token);
+					res.status(201).json({ username: user.username, token });
+				})
+				.catch((err) => {
+					res.status(500).json({ hi: err });
+				});
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
 });
+
+router.post('/login', authenticate, (req, res) => {
+	const creds = req.body;
+	db
+		.findUserName(creds.username)
+		.then((user) => {
+			if (user && bcrypt.compareSync(creds.password, user.password)) {
+				const token = generateToken(user);
+				res.json({ Welcome: user.username, token });
+			} else {
+				res.status(401).json({ message: 'Not Authorized' });
+			}
+		})
+		.catch((err) => {
+			res.status(500).json(err);
+		});
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,12 +3,13 @@ const helmet = require('helmet');
 const cors = require('cors');
 const activities = require('./activities.js');
 const reflections = require('./reflections.js');
-
+const login = require('./login.js');
 const server = express();
 
 server.use(express.json(), helmet(), cors());
 server.use('/activites', activities);
 server.use('/reflections', reflections);
+server.use('/api', login);
 
 server.get('/', (req, res) => {
 	res.status(200).send('Hi!');
