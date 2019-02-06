635bb8005c90a84adbd965b2279c4ae8fdccbe88
travis-russell <russell.travis@yahoo.com>
2019-02-06 12:05:08 -0600

    removed timestamp from seeds

--- data/seeds/activity.js
+++ data/seeds/activity.js
@@ -8,56 +8,49 @@ exports.seed = function(knex, Promise) {
 				fk: 1,
 				enjoymentRating: 4,
 				energyLevel: 3,
-				engagement: 4,
-				timestamp: '02/2 06:15 pm'
+				engagement: 4
 			},
 			{
 				name: 'Journaling',
 				fk: 1,
 				enjoymentRating: 4,
 				energyLevel: 4,
-				engagement: 5,
-				timestamp: '01/30 02:05 pm'
+				engagement: 5
 			},
 			{
 				name: 'TV',
 				fk: 1,
 				enjoymentRating: 3,
 				energyLevel: 2,
-				engagement: 2,
-				timestamp: '01/28 09:20 pm'
+				engagement: 2
 			},
 			{
 				name: 'Biking',
 				fk: 1,
 				enjoymentRating: 3,
 				energyLevel: 3,
-				engagement: 3,
-				timestamp: '01/23 04:45 pm'
+				engagement: 3
 			},
 			{
 				name: 'Swimming',
 				fk: 1,
 				enjoymentRating: 4,
 				energyLevel: 5,
-				engagement: 5,
-				timestamp: '01/13 12:45 pm'
+				engagement: 5
 			},
 			{
 				name: 'Running',
 				fk: 1,
 				enjoymentRating: 2,
 				energyLevel: 3,
-				engagement: 4,
-				timestamp: '02/2 06:15 pm'
+				engagement: 4
 			},
 			{
 				name: 'TV',
 				fk: 1,
 				enjoymentRating: 3,
 				energyLevel: 2,
-				engagement: 4,
-				timestamp: '01/28 09:20 pm'
+				engagement: 4
 			}
 		]);
 	});
