65058bef287dac5daa6536cb07cc393f52970dc9
travis-russell <russell.travis@yahoo.com>
2019-02-06 12:23:46 -0600

    admin can access admin/users

--- api/user.js
+++ api/user.js
@@ -4,7 +4,7 @@ const db = require('../data/helper/userModal');
 const { authenticate } = require('../auth/auth');
 
 router.get('/users', authenticate, (req, res) => {
-	if (currentUser === admin) {
+	if (req.decoded.username === 'admin') {
 		db.getUsers().then((users) => res.status(200).json(users)).catch((err) => {
 			res.status(500).json(`Server error: ${err}`);
 		});
