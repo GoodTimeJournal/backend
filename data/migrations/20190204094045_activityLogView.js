37f9cf4b93716c10eb2971055f7ad3b52697844c
travis-russell <russell.travis@yahoo.com>
2019-02-06 10:52:50 -0600

    energyLevel => energyRating + seeded activities

--- data/migrations/20190204094045_activityLogView.js
+++ data/migrations/20190204094045_activityLogView.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
 		table.increments();
 		table.string('name', 128).notNullable();
 		table.integer('fk').notNullable().references('id').inTable('users');
-		table.integer('enjoymentLevel').notNullable();
+		table.integer('enjoymentRating').notNullable();
 		table.integer('energyLevel').notNullable();
 		table.integer('engagement').notNullable();
 		table.timestamp('timestamp').defaultTo(knex.fn.now());
--- data/seeds/users.js
+++ data/seeds/users.js
@@ -2,7 +2,6 @@ exports.seed = function(knex, Promise) {
 	return knex('users').del().then(function() {
 		return knex('users').insert([
 			{
-				id: 1,
 				username: 'admin',
 				password: process.env.password || 'a',
 				name: 'admin',
