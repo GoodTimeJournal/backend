453851013e42c80a934f7500f1b30b7219539685
travis-russell <russell.travis@yahoo.com>
2019-02-07 17:28:31 -0600

    changed reflection seed name

--- data/seeds/03_reflection.js
+++ data/seeds/03_reflection.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('reflection').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('reflection').insert([
         {
           week: "One",
           fk:  1,
